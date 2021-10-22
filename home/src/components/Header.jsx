f68fadaf0206e3258c0bbe5317142b76d1e872d1
Lucas Jesus <lucas.lsj1711@gmail.com>
2021-10-22 19:54:11 -0300

    fechando dia

--- home/src/components/Header.jsx
+++ home/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import logo from '../assets/logo-germinare.png'
 import './style.css'
-import {FiChevronDown} from 'react-icons/fi'
+import {FiChevronsDown} from 'react-icons/fi'
+import{FaLock} from 'react-icons/fa'
 import {Carousel, CarouselItem} from 'react-bootstrap'
 import Giroto from '../assets/giroto.png'
 import Cleuton from '../assets/cleuton.png'
@@ -13,11 +14,11 @@ const Header = ()=>{
                 <img src={logo} alt="Logo Escola Germinare" className='logo'/>
                 <nav className='navigation'>
                     <ul className='menu'>
-                        <li className='item-menu'>Admissão 2021/2022</li>
+                        <li className='item-menu' className='admissao' >Admissão 2021/2022</li>
                         <li className='item-menu'>Doação</li>
                         <li className='item-menu'>Contato</li>
                         <li className='item-menu'>
-                            Sobre nós <FiChevronDown size={20}/>
+                            Sobre nós
                             <ul className='about-us'>
                                 <li className='item-menu'>Valores</li>
                                 <li className='item-menu'>Estágios</li>
@@ -27,7 +28,7 @@ const Header = ()=>{
                             </ul>
                         </li>
                         <li className='item-menu'>
-                            Colaborador <FiChevronDown/>
+                            <FaLock className='lock'/> Login
                             <ul className='collaborator'>
                                 <li className='item-menu'>Minha biblioteca</li>
                                 <li className='item-menu'>Agenda Edu</li>
@@ -41,15 +42,19 @@ const Header = ()=>{
             <Carousel className='carouselzin'>
                 <CarouselItem interval={8000}>
                     <img src={Giroto} className='carrossel-img'></img>
+                    <FiChevronsDown className='chevrons'/>
                 </CarouselItem>
                 <CarouselItem  interval={5000}>
                     <img src={Cleuton}  className='carrossel-img'></img>
+                    <FiChevronsDown className='chevrons'/>
                 </CarouselItem >
                 <CarouselItem interval={5000}>
                     <img src={Admissao}  className='carrossel-img'></img>
+                    <FiChevronsDown className='chevrons'/>
                 </CarouselItem >
                 <CarouselItem  interval={5000}>
                     <img src={Universidade} className='carrossel-img'></img>
+                    <FiChevronsDown className='chevrons'/>
                 </CarouselItem>
             </Carousel>
             <div className="caneta-azul"></div>
