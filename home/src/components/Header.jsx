8e2042a9493eb40ad03cd5395ca1b560c281ba4a
Lucas Jesus <lucas.lsj1711@gmail.com>
2021-10-22 16:32:11 -0300

    configured carousel

--- home/src/components/Header.jsx
+++ home/src/components/Header.jsx
@@ -1,37 +1,59 @@
 import logo from '../assets/logo-germinare.png'
 import './style.css'
 import {FiChevronDown} from 'react-icons/fi'
+import {Carousel, CarouselItem} from 'react-bootstrap'
+import Giroto from '../assets/giroto.png'
+import Cleuton from '../assets/cleuton.png'
+import Admissao from '../assets/admissao.png'
+import Universidade from '../assets/universidade.png'
 const Header = ()=>{
     return(
-        <header className='header'>
-            <img src={logo} alt="Logo Escola Germinare" className='logo'/>
-            <nav className='navigation'>
-                <ul className='menu'>
-                    <li className='item-menu'>Admissão 2021/2022</li>
-                    <li className='item-menu'>Doação</li>
-                    <li className='item-menu'>Contato</li>
-                    <li className='item-menu'>
-                        Sobre nós <FiChevronDown size={20}/>
-                        <ul className='about-us'>
-                            <li className='item-menu'>Valores</li>
-                            <li className='item-menu'>Estágios</li>
-                            <li className='item-menu'>Certificados</li>
-                            <li className='item-menu'>Governança</li>
-                            <li className='item-menu'>Projeto Acadêmico</li>
-                        </ul>
-                    </li>
-                    <li className='item-menu'>
-                        Colaborador <FiChevronDown/>
-                        <ul className='collaborator'>
-                            <li className='item-menu'>Minha biblioteca</li>
-                            <li className='item-menu'>Agenda Edu</li>
-                            <li className='item-menu'>Árvore de livros</li>
-                        </ul>
-                    </li>
+        <>
+            <header className='header'>
+                <img src={logo} alt="Logo Escola Germinare" className='logo'/>
+                <nav className='navigation'>
+                    <ul className='menu'>
+                        <li className='item-menu'>Admissão 2021/2022</li>
+                        <li className='item-menu'>Doação</li>
+                        <li className='item-menu'>Contato</li>
+                        <li className='item-menu'>
+                            Sobre nós <FiChevronDown size={20}/>
+                            <ul className='about-us'>
+                                <li className='item-menu'>Valores</li>
+                                <li className='item-menu'>Estágios</li>
+                                <li className='item-menu'>Certificados</li>
+                                <li className='item-menu'>Governança</li>
+                                <li className='item-menu'>Projeto Acadêmico</li>
+                            </ul>
+                        </li>
+                        <li className='item-menu'>
+                            Colaborador <FiChevronDown/>
+                            <ul className='collaborator'>
+                                <li className='item-menu'>Minha biblioteca</li>
+                                <li className='item-menu'>Agenda Edu</li>
+                                <li className='item-menu'>Árvore de livros</li>
+                            </ul>
+                        </li>
 
-                </ul>
-            </nav>
-        </header>
+                    </ul>
+                </nav>
+            </header>
+            <Carousel className='carouselzin'>
+                <CarouselItem interval={8000}>
+                    <img src={Giroto} className='carrossel-img'></img>
+                </CarouselItem>
+                <CarouselItem  interval={5000}>
+                    <img src={Cleuton}  className='carrossel-img'></img>
+                </CarouselItem >
+                <CarouselItem interval={5000}>
+                    <img src={Admissao}  className='carrossel-img'></img>
+                </CarouselItem >
+                <CarouselItem  interval={5000}>
+                    <img src={Universidade} className='carrossel-img'></img>
+                </CarouselItem>
+            </Carousel>
+            <div className="caneta-azul"></div>
+        </>
     )
 }
 export default Header
