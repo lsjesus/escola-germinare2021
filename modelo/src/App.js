1bde40502e324b0f0aa53f0082735b38f931e93c
Lucas Jesus <lucas.lsj1711@gmail.com>
2021-10-26 16:33:12 -0300

    finalizando modelo

--- modelo/src/App.js
+++ modelo/src/App.js
@@ -1,6 +1,9 @@
+import Model from "./components/Model";
+import './styles/global.css'
 function App() {
   return (
     <div className="App">
+      <Model/>
     </div>
   );
 }
---
+++ modelo/src/components/Model.jsx
@@ -0,0 +1,7 @@
+import './style.css'
+const Model = ()=>{
+    return(
+        <h1 className='h1'>Aqui está o modelo de HTML em React</h1>
+    )
+}
+export default Model
