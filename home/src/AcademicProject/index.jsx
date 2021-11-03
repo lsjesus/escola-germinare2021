d56a5354761ecd8581fe53acb7dc6635ca775b98
Alexandro Rocha <alexkatsuharu@gmail.com>
2021-11-03 14:55:57 -0300

    v2

--- home/src/AcademicProject/index.jsx
+++ home/src/AcademicProject/index.jsx
@@ -3,7 +3,85 @@ import './style.css'
 const AcademicProject = ()=>{
     return (
         <>
-            
+            <p>
+                A escola Germinare tem como missão formar jovens administradores de empresas e cidadãos por meio de uma educação única e inteiramente gratuita. É uma instituição aprovada pelo MEC – Ministério da Educação – e reconhecida pela UNESCO.
+                O foco principal é formar de maneira efetiva jovens gestores e líderes para o mercado de trabalho, oferecendo ampla preparação com conteúdos dirigidos ao desenvolvimento de habilidades profissionais e a oportunidade de especialização escolhida entre os seguintes negócios: digital, financeiro, varejo, commodities e bens de consumo.
+                Disciplinas distribuídas em áres de conhecimento e academias:
+            </p>
+
+            <div className="dicipline-container-wrapper">
+                <div className="discipline-container">
+                    <h1>Linguagens e códigos</h1>
+                    <hr />
+                    <ul>
+                        <li>Português</li>
+                        <li>Inglês</li>
+                        <li>Condicionamento Físico</li>
+                    </ul>
+                </div>
+                <div className="discipline-container">
+                    <h1>Matemática</h1>
+                    <hr />
+                    <ul>
+                        <li>Matemática</li>
+                        <li>Geometria</li>
+                        <li>Lógico</li>
+                        <li>Estatística</li>
+                    </ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>
+                <div className="discipline-container">
+                    <h1></h1>
+                    <hr />
+                    <ul></ul>
+                </div>   
+            </div>
+
+            <p>
+                O currículo também prevê uma carga intensa voltada para o inglês, pois acredita-se que para a formação de um gestor em negócios, a fluência neste idioma é essencial.
+                Desse modo, um dos pilares centrais do projeto é não subestimar os jovens, por isso os alunos iniciam estágios já no segundo semestre do 9º ano, como forma de aplicar no mundo empresarial os conhecimentos adquiridos em sala de aula. Esta é uma das estratégias da escola para que que os alunos tomem conhecimentos e demandas do mundo real, interessem-se, entendam a importância e dediquem-se para superar as expectativas.
+                A partir do ano de 2015, o Ensino Médio tornou-se técnico. Assim, os alunos, ao término do 3º ano do Ensino Médio, receberão um certificado de Técnico em Administração de empresas.
+            </p>
+
         </>
     )
 }
