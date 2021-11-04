dae46a9c04e9362f734ffa2463ca2888ffceb3aa
Lucas Jesus <lucas.lsj1711@gmail.com>
2021-11-04 13:13:49 -0300

    Revert "finalizando projeto"
    This reverts commit 309d5713ce85eb42c8524d23e68f37faab6a5d6d.

--- modelo/src/App.js
+++ modelo/src/App.js
@@ -1,13 +1,9 @@
+import Model from "./components/Model";
 import './styles/global.css'
-import Footer from "./components/Footer/Footer/Footer"
-import Main from "./components/Main/Main"
-import Nav from './components/Nav/index'
 function App() {
   return (
     <div className="App">
-
-      <Main/>
-
+      <Model/>
     </div>
   );
 }
