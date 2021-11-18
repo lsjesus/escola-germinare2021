a75f70f92d2058428c169ee800de4020b7575ebd
Lucas Jesus <lucas.lsj1711@gmail.com>
2021-11-18 18:05:48 -0300

    arrumando nav bugado

--- main/src/pages/home/components/Academias/index.jsx
+++ main/src/pages/home/components/Academias/index.jsx
@@ -16,10 +16,11 @@ const Academias = ()=>{
             if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 324){
              const nav = document.querySelector('.nav-wrapper')
              nav.style.opacity = 0
-             nav.style.transition = 'opacity .92s ease-in-out'
+             nav.style.transition = 'opacity .92s ease-in-out, 1s z-index .92s ease-in-out'
+             nav.style.zIndex = -100
             } else{
                 const nav = document.querySelector('.nav-wrapper')
-                
+                nav.style.zIndex = 101
                 nav.style.opacity = 1
                 nav.style.transition = 'opacity .92s ease-in-out'
             }
