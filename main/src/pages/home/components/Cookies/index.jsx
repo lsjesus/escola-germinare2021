c1815cdc7b073efa9794053d78492cff90fddd12
Lucas Jesus <lucas.lsj1711@gmail.com>
2021-11-25 08:08:12 -0300

    setting cookies in localstorage

--- main/src/pages/home/components/Cookies/index.jsx
+++ main/src/pages/home/components/Cookies/index.jsx
@@ -1,8 +1,16 @@
 import './style.css'
 import { Link } from 'react-router-dom';
 import { IoIosCloseCircleOutline } from 'react-icons/io'
+import { useEffect } from 'react';
 const Cookies = ()=>{
+    useEffect(()=>{
+        const validation = localStorage.getItem('@cookies')
+        if (validation === 'true'){
+            document.getElementById('cookie-id').style.display = "none";
+        }
+    }, [])
     function validateCookies() {
+        localStorage.setItem('@cookies', 'true')
         document.getElementById('cookie-id').style.display = "none";
     }
     return(
@@ -10,7 +18,9 @@ const Cookies = ()=>{
 
         <div class="cookie-flex">
         
-            <h1 class="cookie-title">Privacidade & Cookies <IoIosCloseCircleOutline className="cookie-icon" onClick={validateCookies}/></h1>
+            <h1 class="cookie-title">Privacidade & Cookies <IoIosCloseCircleOutline className="cookie-icon" onClick={()=>{
+                document.getElementById('cookie-id').style.display = "none";
+            }}/></h1>
 
             <p class="cookie-text">Para melhorar a sua experiência em nossos serviços e recomendar conteúdo de seu interesse, nós utilizamos cookies e tecnologias semelhantes. Ao aceitar, você concorda com a nossa Política de Privacidade.</p>
             
